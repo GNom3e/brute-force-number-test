12c7917f4c66c12a3a8df8585fc93f2a4ac97547
GNom3e <meisacheeseball@gmail.com>
2026-04-09 22:12:13 -0400

    Add files via upload

---
+++ BruteForce.py
@@ -0,0 +1,40 @@
+import string
+import sys
+
+passkey = ''
+found = False
+try_count = 0
+
+def bruteForce(attempt='', length=1):
+    global found, try_count
+    chars = string.ascii_letters + string.digits + string.punctuation
+    if len(attempt) == length:
+        sys.stdout.write(f"\rTrying attempt: {attempt}")
+        sys.stdout.flush()
+        try_count += 1
+        if attempt == passkey:
+            print(f"\n\nPasskey found: {attempt}")
+            print(f"Total tries: {try_count}")
+            found = True
+        return
+    for char in chars:
+        if found:
+            break
+        bruteForce(attempt+char, length)
+
+def findPasskey():
+    if passkey == '':
+        print("Input Required")
+        return
+    length = 1
+    while not found:
+        bruteForce(length=length)
+        length += 1
+
+def main():
+    global passkey
+    passkey = input("Enter Passkey:")
+    findPasskey()
+
+if __name__ == "__main__":
+    main()
